5069662e98be80a0fc8eb3092b2920e2d4234c96
Ryan Hiebert <ryan@ryanhiebert.com>
2014-05-19 18:36:35 -0500

    Allow creating a board without arguments

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -7,3 +7,13 @@ def test_board_init():
     )
     board = Board(state)
     assert board.state == state
+
+def test_default_init():
+    from tictactoe import Board
+    state = (
+        (None, None, None),
+        (None, None, None),
+        (None, None, None),
+    )
+    board = Board()
+    assert board.state == state
--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,5 @@
 class Board:
-    def __init__(self, state):
+    def __init__(self, state=None):
+        if state is None:
+            state = ((None,) * 3,) * 3
         self.state = state
